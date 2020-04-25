27a09e990c07cf211d5fdc26bacb2c17001bcb48
WTF_ATuan <jain60501@gmail.com>
2020-04-25 12:15:36 +0800

    update: funtion of SwitchBullet and AutoSwitch

---
+++ VR co-Editing/Assets/ATuan/ATuan Script/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet 
+{
+    public string Name;
+    public bool IsFire;
+    public GameObject gameObject;
+    public Bullet(bool a_isFire , string a_Name , GameObject a_gameobject) {
+        Name = a_Name;
+        IsFire = a_isFire;
+        gameObject = a_gameobject;
+    }
+}
