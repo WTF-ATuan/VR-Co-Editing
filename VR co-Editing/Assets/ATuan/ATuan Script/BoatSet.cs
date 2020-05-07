54c76901b75cd4c0c29dfce89bf4ebef062e87f7
WTF_ATuan <jain60501@gmail.com>
2020-05-07 22:22:11 +0800

    Update: Boat and Collider of Word

---
+++ VR co-Editing/Assets/ATuan/ATuan Script/BoatSet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSet : MonoBehaviour
+{
+    public GameObject Boat;
+    public Rigidbody BoatRig;
+    public float Speed;
+    public void Update() {
+        BoatRig.velocity = new Vector3(0, 0, Speed);
+
+    }
+
+}
