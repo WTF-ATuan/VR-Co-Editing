9c1ae1155ccc7153c8644b8d5fa4c35a9d176ed3
MengTa <4a5k0054@stust.edu.tw>
2020-05-30 19:34:55 +0800

    MengTa0530
    19:34

---
+++ Assets/[ATuan]/ATuan Script/Fire.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Fire : MonoBehaviour {
+    public static Fire fireManager;
+    public void Awake()
+    {
+        fireManager = this;
+    }
+    public GameObject Bullet;
+    public float BulletSpeed;
+    public Transform BarrelPivot;
+    public GameObject MuzzlerLash;
+    public List<Bullet> Magazine;
+    public int FullOfMagazine;
+    public GameObject[] PrefabJapan = new GameObject[6];
+
+    private Bullet WordBullet;
+    private int IndexOfBullet = 0;
+    private string[] BullectName = new string[6];
+    private Animator animator;
+    void Start() {
+        animator = GetComponent<Animator>();
+        MuzzlerLash.SetActive(false);
+        SetBullet();
+    }
+    public void ChangeBullectPlus() {
+        IndexOfBullet++;
+        if (IndexOfBullet >= Magazine.Count)
+        {
+            IndexOfBullet = 0;
+        }
+    }
+    public void ChangeBullectMinus() {
+        IndexOfBullet--;
+        if (IndexOfBullet >= Magazine.Count || IndexOfBullet < 0)
+        {
+            IndexOfBullet = 0;
+        }
+    }
+    public void InputOFComputer() {
+        if (Input.GetMouseButtonDown(0)) {
+            OpenFIre();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            IndexOfBullet++;
+            if (IndexOfBullet >= Magazine.Count) {
+                IndexOfBullet = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            IndexOfBullet--;
+            if (IndexOfBullet >= Magazine.Count) {
+                IndexOfBullet = 0;
+            }
+        }
+    }
+    public List<Bullet> SetBullet() {
+        Magazine = new List<Bullet>();
+        for (int index = 0; index < PrefabJapan.Length; index++) {
+            WordBullet = new Bullet(false, PrefabJapan[index].name, PrefabJapan[index]);
+            Magazine.Add(WordBullet);
+        }
+        return Magazine;
+
+    }
+    public void OpenFIre() {
+        Debug.Log("Fireing");
+        animator.SetTrigger("OpenFire");
+        GameObject BulletFromNow = Instantiate(Magazine[IndexOfBullet].gameObject, BarrelPivot.position, Quaternion.identity);
+        Rigidbody bulletRb = BulletFromNow.GetComponent<Rigidbody>();
+        BulletFromNow.name = Magazine[IndexOfBullet].Name;
+        bulletRb.velocity = BarrelPivot.forward * BulletSpeed;
+        MuzzlerLash.SetActive(true);
+        IndexOfBullet++;
+        if (IndexOfBullet >= Magazine.Count) {
+            IndexOfBullet = 0;
+        }
+    }
+
+}
---
+++ Assets/[Eva]/Eva Script/ViveInput.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+
+
+public class ViveInput : MonoBehaviour
+{
+    private Interactable interactable;
+    private Valve.VR.InteractionSystem.Hand hand;
+
+    public SteamVR_Action_Boolean GripFireAction;
+    public SteamVR_Action_Boolean GrapPinchAction;
+    public SteamVR_Action_Boolean SnapTurnLeft = SteamVR_Input.GetBooleanAction("TurnLeft");
+    public SteamVR_Action_Boolean SnapTurnRight = SteamVR_Input.GetBooleanAction("TurnRight");
+    public SteamVR_Action_Boolean IsGrapingGun;
+    //public SteamVR_Action_Boolean snapRightAction = SteamVR_Input.GetBooleanAction("SnapTurnRight");
+
+    private void Awake()
+    {
+        interactable = GetComponent<Interactable>();
+        hand = new Valve.VR.InteractionSystem.Hand();
+    }
+
+    private void FixedUpdate()
+    {
+        InputOfVRSet();
+    }
+
+    public void InputOfVRSet()
+    {
+        //開槍Input
+        if (GripFireAction.GetLastStateDown(SteamVR_Input_Sources.RightHand)/* && hand.currentAttachedObject.name == "Loud_public" */) //讀取FireActicon GrapGrip值  
+        {
+            //OpenFIre();
+            Debug.Log("OpenFIreVRInput");
+            Fire.fireManager.OpenFIre();
+
+        }
+        //拿取物件Input
+        if (GrapPinchAction.GetLastStateDown(SteamVR_Input_Sources.Any)) //讀取GrapPinchAction GrapPinch值  
+        {
+
+            Debug.Log("GrapPinchActionVRInput");
+
+        }
+
+        //搖桿左
+        if (SnapTurnLeft.GetLastStateDown(SteamVR_Input_Sources.Any))
+        {
+            Debug.Log("TurnLeftActionVRInput");
+            Fire.fireManager.ChangeBullectMinus();
+
+        }
+
+        //搖桿右
+        if (SnapTurnRight.GetLastStateDown(SteamVR_Input_Sources.Any))
+        {
+            Debug.Log("TurnRightActionVRInput");
+            Fire.fireManager.ChangeBullectPlus();
+
+        }
+
+    }
+}
