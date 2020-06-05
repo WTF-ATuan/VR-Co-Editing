05fd75939ffe82a3c953b088a71caea4864cb4bf
MengTa <4a5k0054@stust.edu.tw>
2020-06-05 14:51:34 +0800

    Update: goooood BoatSet
    123456123456

--- Assets/[ATuan]/ATuan Script/BoatSet.cs
+++ Assets/[ATuan]/ATuan Script/BoatSet.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class BoatSet : MonoBehaviour
 {
-    public GameObject Boat;
-    public Transform BoatPosition;
-    public Rigidbody BoatRig;
     [SerializeField] private float Speed;
     [SerializeField] private float MaxDistance;
-    public void Start() {
-        BoatRig.velocity = new Vector3(0, 0, Speed);
-    }
-    public void Update() {
-        RaycastHit hit;
-        Ray BoatRay = new Ray(BoatPosition.position, Vector3.forward);
-        //Debug.DrawRay(BoatPosition.position, Vector3.forward , Color.red , MaxDistance);
-        if (Physics.Raycast(BoatRay, out hit, 50f)) {
-            //Debug.Log(hit.collider.gameObject.tag);
-            if (hit.collider.gameObject.tag == "Trigger" && hit.distance < MaxDistance) {
-                BoatRig.velocity = Vector3.zero;
-               
-            } else
-                BoatRig.velocity = new Vector3(0, 0, Speed);
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+    public void Update()
+    {
 
-        } else
-            BoatRig.velocity = new Vector3(0, 0, Speed);
     }
-
+    public void BoatMove(Vector3 TargetPosition, float SmoothTime)
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, TargetPosition, ref velocity, SmoothTime);
+    }
 }
+    
