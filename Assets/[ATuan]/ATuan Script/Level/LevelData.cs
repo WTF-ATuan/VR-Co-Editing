247f6913d688a2a56b0464787161ebe901e5670b
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2020-10-05 12:00:55 +0800

    privous version
    123

---
+++ Assets/[ATuan]/ATuan Script/Level/LevelData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData
+{
+    public string LevelName;
+    public BulletSet LevelBullet;
+    public int Level;
+    public LevelData() {
+    
+    
+    }
+    
+}
